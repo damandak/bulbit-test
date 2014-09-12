f06a438e0fb00a556891d767f14399339ea55def
damandak <damandak@uc.cl>
2014-09-11 23:33:42 -0300

    Commit inicial, solo un poco de la página de inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bulbit::Application.config.secret_key_base = 'e81118e311ec8de4e60e6c26e82c9a7dfda9174a563609f610ecca895d2af72ec2888216cc3ba634b3814a4a85ffc4c2033292244cca0b423da8081863dd1e0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bulbit::Application.config.session_store :cookie_store, key: '_bulbit_session'
